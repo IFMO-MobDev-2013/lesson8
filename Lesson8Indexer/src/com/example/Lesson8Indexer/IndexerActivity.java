1df14d796b13078a536791c357db9c3be8db9e89
Genyaz <genya12321@gmail.com>
2013-11-01 15:43:04 +0400

    Exception handling.

--- Lesson8Indexer/src/com/example/Lesson8Indexer/IndexerActivity.java
+++ Lesson8Indexer/src/com/example/Lesson8Indexer/IndexerActivity.java
@@ -47,66 +47,72 @@ public class IndexerActivity extends Activity {
     public void onIndexing(View view) {
         successView.setText("Indexing...");
         informationView.setText("Please, wait for results.");
-        int notesCount = 0;
-        int wordsCount = 0;
-        ContentResolver contentResolver = getContentResolver();
-        Cursor cursor = contentResolver.query(URI_NOTES, null, null, null, null);
-        String s;
-        String[] ss;
-        StringBuilder sb;
-        char c;
-        dictionary = new Hashtable<String, Integer>();
-        int textColumn = cursor.getColumnIndex(T_NOTES_TEXT);
-        while (cursor.moveToNext()) {
-            notesCount++;
-            s = cursor.getString(textColumn);
-            sb = new StringBuilder();
-            for (int i = 0; i < s.length(); i++) {
-                c = s.charAt(i);
-                if (((c <= 'z') && (c >= 'a')) || ((c <= 'Z') && (c >= 'A')) || ((c <= '9') && (c >= '0'))) {
-                    sb.append(c);
-                } else {
-                    sb.append(" ");
-                }
-            }
-            ss = sb.toString().split("\\s");
-            for (int i = 0; i < ss.length; i++) {
-                if (!ss[i].isEmpty()) {
-                    ss[i] = ss[i].toLowerCase();
-                    if (!dictionary.containsKey(ss[i])) {
-                        dictionary.put(ss[i], 1);
-                        wordsCount++;
+        try {
+            int notesCount = 0;
+            int wordsCount = 0;
+            ContentResolver contentResolver = getContentResolver();
+            Cursor cursor = contentResolver.query(URI_NOTES, null, null, null, null);
+            String s;
+            String[] ss;
+            StringBuilder sb;
+            char c;
+            dictionary = new Hashtable<String, Integer>();
+            int textColumn = cursor.getColumnIndex(T_NOTES_TEXT);
+            while (cursor.moveToNext()) {
+                notesCount++;
+                s = cursor.getString(textColumn);
+                sb = new StringBuilder();
+                for (int i = 0; i < s.length(); i++) {
+                    c = s.charAt(i);
+                    if (((c <= 'z') && (c >= 'a')) || ((c <= 'Z') && (c >= 'A')) || ((c <= '9') && (c >= '0'))) {
+                        sb.append(c);
                     } else {
-                        Integer integer = dictionary.get(ss[i]);
-                        dictionary.put(ss[i], integer + 1);
+                        sb.append(" ");
+                    }
+                }
+                ss = sb.toString().split("\\s");
+                for (int i = 0; i < ss.length; i++) {
+                    if (!ss[i].isEmpty()) {
+                        ss[i] = ss[i].toLowerCase();
+                        if (!dictionary.containsKey(ss[i])) {
+                            dictionary.put(ss[i], 1);
+                            wordsCount++;
+                        } else {
+                            Integer integer = dictionary.get(ss[i]);
+                            dictionary.put(ss[i], integer + 1);
+                        }
                     }
                 }
             }
-        }
-        cursor.close();
-        Enumeration<String> enumeration = dictionary.keys();
-        int min = -1;
-        int max = -1;
-        String maxWord = "NO_SUCH_WORD";
-        String minWord = "NO_SUCH_WORD";
-        while (enumeration.hasMoreElements()) {
-            s = enumeration.nextElement();
-            Integer integer = dictionary.get(s);
-            if ((max == -1) || (max < integer)) {
-                max = integer;
-                maxWord = s;
+            cursor.close();
+            Enumeration<String> enumeration = dictionary.keys();
+            int min = -1;
+            int max = -1;
+            String maxWord = "NO_SUCH_WORD";
+            String minWord = "NO_SUCH_WORD";
+            while (enumeration.hasMoreElements()) {
+                s = enumeration.nextElement();
+                Integer integer = dictionary.get(s);
+                if ((max == -1) || (max < integer)) {
+                    max = integer;
+                    maxWord = s;
+                }
+                if ((min == -1) || (min > integer)) {
+                    min = integer;
+                    minWord = s;
+                }
+                adapter.add("Word = \"" + s + "\", count = " + integer + ".");
+                adapter.notifyDataSetChanged();
             }
-            if ((min == -1) || (min > integer)) {
-                min = integer;
-                minWord = s;
-            }
-            adapter.add("Word = \"" + s + "\", count = " + integer + ".");
-            adapter.notifyDataSetChanged();
+            Log.w("maxWord", "Word with max count is \"" + maxWord + "\", count = " + max + ".");
+            Log.w("minWord", "Word with min count is \"" + minWord + "\", count = " + min + ".");
+            successView.setText("The notes has been indexed.");
+            informationView.setText(notesCount + " notes have been indexed, " + wordsCount + " different words in these notes.");
+        } catch (Exception e) {
+            successView.setText("Bad database.");
+            informationView.setText(e.getMessage());
         }
-        Log.w("maxWord", "Word with max count is \"" + maxWord + "\", count = " + max + ".");
-        Log.w("minWord", "Word with min count is \"" + minWord + "\", count = " + min + ".");
-        successView.setText("The notes has been indexed.");
-        informationView.setText(notesCount + " notes have been indexed, " + wordsCount + " different words in these notes.");
+
     }
 
     @Override
