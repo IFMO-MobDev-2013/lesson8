6bdc06ccfa84814bc78cd4379393b92d611d7b1e
Genyaz <genya12321@gmail.com>
2013-11-01 15:15:04 +0400

    First part of the task.
    Please, distribute scores between Menshov Sergey, 2537 and Varlamov
    Evgeny, 2539.

---
+++ Lesson8Indexer/src/com/example/Lesson8Indexer/IndexerActivity.java
@@ -0,0 +1,112 @@
+package com.example.Lesson8Indexer;
+
+import android.app.Activity;
+import android.content.ContentResolver;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.Enumeration;
+import java.util.Hashtable;
+import java.util.Vector;
+
+
+public class IndexerActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+
+    private TextView successView;
+    private TextView informationView;
+    private ListView listView;
+    private ArrayAdapter<String> adapter;
+    private Hashtable<String, Integer> dictionary;
+
+    public static final String STRUCTURE_NOTES = "notes";
+    public static final String AUTHORITY = "org.nottingham.provider";
+
+    private static final Uri CONTENT_URI = Uri.parse("content://" + AUTHORITY);
+    public static final Uri URI_NOTES = Uri.withAppendedPath(CONTENT_URI, STRUCTURE_NOTES);
+
+    private static final String  T_NOTES = "Notes";
+
+    public static final String T_NOTES_ID = "note_id";
+    public static final String T_NOTES_TEXT = "text";
+    public static final String T_NOTES_UPDATED = "updated";
+
+
+
+
+    public void onIndexing(View view) {
+        successView.setText("Indexing...");
+        informationView.setText("Please, wait for results.");
+        int notesCount = 0;
+        int wordsCount = 0;
+        ContentResolver contentResolver = getContentResolver();
+        Cursor cursor = contentResolver.query(URI_NOTES, null, null, null, null);
+        String s;
+        String[] ss;
+        StringBuilder sb;
+        char c;
+        dictionary = new Hashtable<String, Integer>();
+        int textColumn = cursor.getColumnIndex(T_NOTES_TEXT);
+        while (cursor.moveToNext()) {
+            notesCount++;
+            s = cursor.getString(textColumn);
+            sb = new StringBuilder();
+            for (int i = 0; i < s.length(); i++) {
+                c = s.charAt(i);
+                if (((c <= 'z') && (c >= 'a')) || ((c <= 'Z') && (c >= 'A')) || ((c <= '9') && (c >= '0'))) {
+                    sb.append(c);
+                } else {
+                    sb.append(" ");
+                }
+            }
+            ss = sb.toString().split("\\s");
+            for (int i = 0; i < ss.length; i++) {
+                if (!ss[i].isEmpty()) {
+                    ss[i] = ss[i].toLowerCase();
+                    if (!dictionary.containsKey(ss[i])) {
+                        dictionary.put(ss[i], 1);
+                        wordsCount++;
+                    } else {
+                        Integer integer = dictionary.get(ss[i]);
+                        dictionary.put(ss[i], integer + 1);
+                    }
+                }
+            }
+        }
+        cursor.close();
+        Enumeration<String> enumeration = dictionary.keys();
+        while (enumeration.hasMoreElements()) {
+            s = enumeration.nextElement();
+            Integer integer = dictionary.get(s);
+            adapter.add("Word = \"" + s + "\", count = " + integer + ".");
+            adapter.notifyDataSetChanged();
+        }
+        successView.setText("The notes has been indexed.");
+        informationView.setText(notesCount + " notes have been indexed, " + wordsCount + " different words in these notes.");
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        successView = (TextView) findViewById(R.id.success);
+        successView.setText("Ready to start indexing.");
+        successView.setTextSize(20);
+        informationView = (TextView) findViewById(R.id.information);
+        informationView.setText("No notes has been indexed by now.");
+        informationView.setTextSize(20);
+        listView = (ListView) findViewById(R.id.listView);
+        adapter = new ArrayAdapter<String>(this, R.layout.text);
+        listView.setAdapter(adapter);
+    }
+}
---
+++ Lesson8Indexer/src/com/example/Lesson8Indexer/IndexerProvider.java
@@ -0,0 +1,45 @@
+package com.example.Lesson8Indexer;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.net.Uri;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Genyaz
+ * Date: 01.11.13
+ * Time: 14:00
+ * To change this template use File | Settings | File Templates.
+ */
+public class IndexerProvider extends ContentProvider {
+    @Override
+    public boolean onCreate() {
+        return false;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public String getType(Uri uri) {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public Uri insert(Uri uri, ContentValues values) {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public int delete(Uri uri, String selection, String[] selectionArgs) {
+        return 0;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
+        return 0;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+}
